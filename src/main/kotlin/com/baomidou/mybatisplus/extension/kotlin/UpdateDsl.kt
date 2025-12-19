eccb70c0e1fd77cbc6ae4052b75152d24565d2f4
lidiwei <lidiwei@wakedata.com>
2025-12-19 17:35:51 +0800

    3.5.15.5

--- src/main/kotlin/com/baomidou/mybatisplus/extension/kotlin/UpdateDsl.kt
+++ src/main/kotlin/com/baomidou/mybatisplus/extension/kotlin/UpdateDsl.kt
@@ -10,10 +10,10 @@ import kotlin.reflect.KProperty1
 interface UpdateDsl<Children, T> : Update<Children, KProperty1<in T, *>> {
     infix fun <V> KProperty1<in T, V>.set(value: V?) = this@UpdateDsl.set(this, value)
 
-    infix fun <V> KProperty1<in T, String?>.setIfNotBlank(value: String?) =
+    infix fun <V> KProperty1<in T, V?>.setIfNotBlank(value: String?) =
         this@UpdateDsl.set(value?.isNotBlank() == true, this, value)
 
-    infix fun <V> KProperty1<in T, String?>.setIfNotEmpty(value: String?) =
+    infix fun <V> KProperty1<in T, V?>.setIfNotEmpty(value: String?) =
         this@UpdateDsl.set(value?.isNotEmpty() == true, this, value)
 
     infix fun <V> KProperty1<in T, V>.setIfNotNull(value: V?) = this@UpdateDsl.set(value != null, this, value)
